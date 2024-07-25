13078533ada81ef68c770a96b03203faffd73473
fadhil9 <fadhilmuhamad272@gmail.com>
2024-07-25 16:00:30 +0700

    update

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami',(req,res)=>{
+
+  let ip = req.ip;
+
+  if(ip.startsWith('::ffff:')){
+    ip = ip.split(":").pop();
+  }
+  
+  const bahasa = req.headers['accept-language']
+  const p_lunak = req.headers['user-agent']
+  
+  res.json({ipaddress : ip, language : bahasa, software : p_lunak});
+  })
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
